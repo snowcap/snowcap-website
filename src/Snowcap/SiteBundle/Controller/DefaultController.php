75561fcad2746ac856b7f6f688d86c4e8d1c6e67
Jerome Poskin <jerome@snowcap.be>
2011-06-26 13:35:13 +0200

    Getting post from database and display the on homepage

--- src/Snowcap/SiteBundle/Controller/DefaultController.php
+++ src/Snowcap/SiteBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Snowcap\SiteBundle\Entity\Post;
+
 class DefaultController extends Controller
 {
     /**
@@ -14,6 +16,32 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return array();
+		$em = $this->getDoctrine()->getEntityManager();
+
+        $entities = $em->getRepository('SnowcapSiteBundle:Post')->findAll();
+
+        return array('entities' => $entities);    
+	}
+
+	/**
+     * Finds and displays a Post entity.
+     *
+     * @Route("/post/{id}", name="postShow")
+     * @Template()
+     */
+    public function postShowAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('SnowcapSiteBundle:Post')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Post entity.');
+        }
+
+        return array(
+            'entity'      => $entity,
+        );
     }
+
 }
