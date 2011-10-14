39a626f922664aa3a361c564cca441948d24788b
Jerome Poskin <jerome@snowcap.be>
2011-10-14 07:03:50 +0200

    Removing vendors

--- vendor/bundles/Knplabs/Bundle/MarkdownBundle/Tests/Performance/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$name = 'Min';
-
-require_once __DIR__.'/'.$name.'.php';
-$class = 'Knplabs\\MarkdownBundle\\Tests\\Performance\\'.$name;
-$test = new $class();
-print nl2br($test->run(10));
