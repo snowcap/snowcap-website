0129349bf852ee66a6f58c3638977bc4ad244e0f
Pierre Vanliefland <pierre@snowcap.be>
2011-10-18 11:10:49 +0200

    magic getter for entities (underscore to camelcase)

--- src/Snowcap/SiteBundle/Entity/Content.php
+++ src/Snowcap/SiteBundle/Entity/Content.php
@@ -1,6 +1,29 @@
 <?php
-
 namespace Snowcap\SiteBundle\Entity;
 
-abstract class Content {
+use Symfony\Component\DependencyInjection\Container;
+
+/**
+ * Abstract content class
+ *
+ */
+abstract class Content
+{
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return method_exists($this, 'get' . ucfirst(Container::camelize($key)));
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        return call_user_func(array($this, 'get' . ucfirst(Container::camelize($key))));
+    }
 }
