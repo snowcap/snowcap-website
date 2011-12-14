eda8b13d79c3449f2c822627b70524cc36f4ce4b
Jerome Poskin <jerome@snowcap.be>
2011-12-14 16:32:51 +0100

    Styling blog comments

--- src/Snowcap/SiteBundle/Form/CommentType.php
+++ src/Snowcap/SiteBundle/Form/CommentType.php
@@ -10,9 +10,9 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder
-            ->add('name')
-			->add('email', 'email')
-            ->add('body')
+            ->add('name', 'text', array('label' => 'blog.comment.name'))
+			->add('email', 'email', array('label' => 'blog.comment.email'))
+            ->add('body', 'textarea', array('label' => 'blog.comment.body'))
         ;
     }
 	
