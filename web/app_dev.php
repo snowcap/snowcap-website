bc84c552d2982e9828f82ba274f79ed957974931
Jérôme Poskin <jerome@snowcap.be>
2011-12-29 16:57:28 +0100

    * Moving up background for services * Putting debug to dev

--- web/app_dev.php
+++ web/app_dev.php
@@ -19,6 +19,6 @@ require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-$kernel = new AppKernel('dev', false);
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 $kernel->handle(Request::createFromGlobals())->send();
